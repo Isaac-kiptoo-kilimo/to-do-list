46b82a4e75ce41d0d2836a371725771f036b8a93
Isaac-kiptoo-kilimo <isaac.kilimo@student.moringaschool.com>
2022-04-07 11:39:12 +0300

    feature:our to do list

---
+++ src/app/task/task.component.ts
@@ -0,0 +1,28 @@
+import { Component, OnInit } from '@angular/core';
+import { Task } from '../task';
+@Component({
+  selector: 'app-task',
+  templateUrl: './task.component.html',
+  styleUrls: ['./task.component.css']
+})
+export class TaskComponent implements OnInit {
+  toogleDetails(index){
+    this.tasks[index].showDescription=!this.tasks[index].showDescription;
+  }
+
+  tasks:Task[]= [
+    new Task(1,"group work" , "Learning Typescript"),
+    new Task(2,"One on one session" ,"Addressing blockers"),
+    new Task(3,"feedback session" ," Giving each other feedback on how the week was"),
+    new Task(4,"Career talk" ,"How to write cv"),
+    new Task(5,"Assignment" ,"Completing our IPs"),
+
+
+  ]
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
